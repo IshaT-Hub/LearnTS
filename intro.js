6f4126d5095cc929527506939d5f2a5fb69ca149
IshaT-Hub <ishatiwari0508@gmail.com>
2024-12-19 18:11:30 +0530

    new

---
+++ intro.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    //num.toUpperCase();
+    return num + 2;
+}
+function signUpUser(name, email, idPaid) {
+    if (idPaid === void 0) { idPaid = false; }
+}
+var LoginUser = function (name, email, idPaid) {
+    if (idPaid === void 0) { idPaid = false; }
+};
+addTwo(5);
+signUpUser("isha", "i@i.com");
+LoginUser("ish", "ish@g.com");
---
+++ intro.ts
@@ -0,0 +1,14 @@
+function addTwo(num: number):number{
+    //num.toUpperCase();
+    return num+2;
+}
+
+function signUpUser(name: string, email:string, idPaid: boolean = false){}
+
+let LoginUser = (name: string, email:string, idPaid: boolean = false) =>{}
+
+addTwo(5);
+signUpUser("isha","i@i.com");
+LoginUser("ish", "ish@g.com")
+export{}
+
